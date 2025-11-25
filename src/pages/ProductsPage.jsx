dd4bace0591d62361dbbae994b47c9768d8dacf5
Marcoz-VS <marcozviniciussilva@gmail.com>
2025-11-25 08:18:48 -0300

    Fiz o filtro e a pesquisa

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,20 @@
 import useProducts from "../hooks/useProduct";
 import ProductCard from "../components/ProductCard";
+import { useState } from "react";
 
 export default function ProductsPage() {
   const { products, loading, error } = useProducts();
+  const [ search, setSearch ] = useState("");
+  const [ filter, setFilter ] = useState("all");
 
   if (loading) return <p>Carregando...</p>;
   if (error) return <p>Erro ao carregar produtos.</p>;
+
+  const filtrado = products.filter((p) => {
+    if (filter === "all") return true;
+    return p.category === filter;
+    }).filter((p) => p.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div style={{
@@ -13,7 +22,21 @@ export default function ProductsPage() {
       flexWrap: "wrap",
       gap: "20px"
     }}>
-      {products.map(prod => (
+
+      <input
+        placeholder="Pesquisar"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+        <option value="all">Todos</option>
+        <option value="men's clothing">Masculino</option>
+        <option value="women's clothing">Feminino</option>
+        <option value="jewelery">Bijuterias</option>
+      </select>
+
+      {filtrado.map(prod => (
         <ProductCard key={prod.id} product={prod} />
       ))}
     </div>
