1ff01a117306d49ea5e2016fd0a43ff6ef175447
sammyrasa <samyra.negreiros09@gmail.com>
2025-12-02 10:40:03 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import 'primereact/resources/themes/lara-light-teal/theme.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 import 'primereact/resources/primereact.min.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
